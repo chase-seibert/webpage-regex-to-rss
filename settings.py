6a03a6726c1d2f9e2a999a5a79b228f56604c423
Chase Seibert <cseibert@dropbox.com>
2021-03-20 21:23:24 -0700

    Regex tweaks for bogleheads

--- settings.py
+++ settings.py
@@ -7,10 +7,10 @@ FEEDS = {
         'id': 'https://s3-us-west-2.amazonaws.com/rsscombine/bogleheads2.xml',
         'title': 'Bogleheads Personal Finance, topics with over 50 replies',
         'parse': {
-            'entry': lambda x: x.find_all('li', class_='row bg1'),
+            'entry': lambda x: x.find_all('li', class_='row'),
             'title': lambda x: x.find('a', class_='topictitle').text,
             'link': r'./viewtopic.php.*?t=[0-9]+',
-            'date': r'» ([A-Za-z]{3} [A-Za-z]{3} [0-9]{1,2}, [0-9]{4} [0-9]{1,2}:[0-9]{2} ..)',
+            'date': r'([A-Za-z]{3} [A-Za-z]{3} [0-9]{1,2}, [0-9]{4} [0-9]{1,2}:[0-9]{2} ..)',
             'content': lambda x: '',
             # extra
             'replies': r'Replies: <strong>([0-9]+)'
